83b8911afa3b4a2345777fbeff6376aaeac1042b
John Smith <example@stanford.edu>
2013-08-02 00:24:44 +0000

    cambio algun texto

--- grader.js
+++ grader.js
@@ -24,7 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var rest = require('restler');
+//var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -58,17 +59,41 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
-    return fn.bind({});
+	return fn.bind({});
+}
+
+
+var buildfn = function(checks) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            fs.writeFileSync('kk.txt', result);
+            var checkJson = checkHtmlFile('kk.txt', checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+    	    console.log(outJson);
+	}
+    };
+    return response2console;
 };
 
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <html_file>','URL to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    if (program.file) {
+     var checkJson = checkHtmlFile(program.file, program.checks);
+     var outJson = JSON.stringify(checkJson, null, 4);
+	//console.log(program.file);console.log('file');
+     console.log(outJson);
+    }
+    if (program.url) {
+     var response2console = buildfn(program.checks);
+     rest.get(program.url).on('complete', response2console);
+    }	
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
